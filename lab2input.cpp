b8317827dcab88510bef8e406dc0c045aba1f29c
ThiriZueZun <684244101@mail.pbru.ac.th>
2025-07-15 11:20:46 +0700

    Create lab2input.cpp

---
+++ lab2input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x;
+	cout << "Hello world ---";
+	cout << endl << "Input a number : ";
+	cin >> x;
+	cout << "Your input x = " << x << endl;
+}
