cbabbbe1e687292f16eaba38a3b548f30469504e
ThiriZueZun <684244101@mail.pbru.ac.th>
2025-08-02 15:11:37 +0700

    Create lab_20-triangle,rectangle&circle.hpp

---
+++ lab_20-triangle,rectangle&circle.hpp
@@ -0,0 +1,60 @@
+//filename : lab 20-triangle.hpp
+
+class Triangle {
+	public:
+	float base, height;
+	
+	void setTriangle(float b, float h){
+		base = b;
+		height = h;
+	}
+	
+	void perimeter() {
+		float c;
+		c = std::sqrt((base*base)+(height*height));
+		std::cout << "Perimeter of triangle = "<< (base + height + c) << std::endl;
+	}
+	
+	void area() {
+	float area;
+	area = 0.5f * base * height;
+	std::cout << "Area of triangle = " << area << std::endl;
+		
+	}
+};
+
+class Rectangle{
+	public:
+		int width,length;
+	
+		void setRectangle(int w,int l){
+	width = w;
+	length = l;
+	}
+
+	void perimeter(){
+	std::cout << "Perimeter of rectangle is " << (2*(width+length)) << std::endl;
+	}
+
+	void area(){
+	std::cout << "Area of rectangle is " << (width*length) << std::endl;
+	}
+	
+};
+
+class Circle{
+	public:
+		float radius;
+		
+		void setCircle(float r){
+			radius = r;
+		}
+		
+		void perimeter(){
+			std::cout << "Perimeter of circle is " << (2*3.14*radius) << std::endl;
+		}
+		
+		void area(){
+			std::cout << "Area of circle is " << (2*3.14*pow(radius,2)) << std::endl;
+		}
+};
